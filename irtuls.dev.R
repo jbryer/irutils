b18fe0ec5d65babfc92d851706ad630bede8ca85
Jason Bryer <jason@bryer.org>
2011-11-10 21:23:47 -0500

    Updated docs.

--- irtuls.dev.R
+++ irtuls.dev.R
@@ -1,13 +1,17 @@
+install.packages(c('devtools', 'roxygen2', 'RSQLite', 'ipeds'), 
+		repos=c('http://cran.r-project.org', 'http://r-forge.r-project.org'))
+
 require(devtools)
 require(roxygen2)
 require(RSQLite)
 require(ipeds)
 
-setwd("~/Dropbox/Projects/irutils")
-setwd("C:/Dropbox/My Dropbox/Projects")
+setwd("~/Dropbox/Projects") #Mac
+setwd("C:/Dropbox/My Dropbox/Projects") #Windows
 
 #Package building
 document("irutils", clean=TRUE)
+document("irutils")
 check_doc("irutils")
 build("irutils", binary=FALSE)
 build("irutils", binary=TRUE)
@@ -16,38 +20,23 @@ check("irutils")
 library(irutils)
 ls('package:irutils')
 
+#Build Vignette
+setwd("C:/Dropbox/My Dropbox/Projects/irutils")
+setwd(paste(getwd(), '/man/doc/', sep=''))
+getwd()
+Stangle('irutils.Rnw')
+Sweave('irutils.Rnw')
+texi2dvi('irutils.tex', pdf=TRUE)
+
+#Load included data
+data(pisa)
+
 
 #Setup the SQLite database
 data(surveys)
 drv = dbDriver("SQLite")
 conn = dbConnect(drv, dbname=paste(getwd(), '/irutils/data/ipeds.db', sep=''))
-dbWriteTable(conn, "surveys", surveys, row.names=0)
-saveIPEDStoDB <- function(conn, surv=surveys$SurveyID, years=2010:2006) {
-	for(s in surv) {
-		if(dbExistsTable(conn, s)) {
-			dbRemoveTable(conn, s)
-		}
-		for(y in years[order(years, decreasing=TRUE)]) {
-			tab = NULL
-			tab = tryCatch(getIPEDSSurvey(s, y), error=function(e) { print(paste("Error downloading",s,"for year",y)); return(NULL) } )
-			if(!is.null(tab)) {
-				tab$year = y
-				if(dbExistsTable(conn, s)) {
-					fields = dbListFields(conn, s)
-					tab = tab[,names(tab) %in% fields]
-					tab[,fields[!fields %in% names(tab)]] = NA
-					tab = tab[,fields]
-					dbWriteTable(conn, s, tab, row.names=FALSE, append=TRUE)
-				} else {
-					dbWriteTable(conn, s, tab, row.names=FALSE, append=FALSE)
-				}
-			}
-		}
-	}
-}
-
 saveIPEDStoDB(conn, surv=surveys$SurveyID, years=2010:2006)
-
 dbListTables(conn)
 
 dbGetQuery(conn, "SELECT SurveyID, Title FROM surveys")
@@ -56,14 +45,6 @@ hd = dbReadTable(conn, 'HD')
 table(hd$year, useNA='ifany')
 dbGetQuery(conn, "SELECT SurveyID, Title FROM surveys")
 
-
 dbDisconnect(conn)
 
-#Build Vignette
-setwd("C:/Dropbox/My Dropbox/Projects/irutils")
-setwd(paste(getwd(), '/man/doc/', sep=''))
-getwd()
-Stangle('irutils.Rnw')
-Sweave('irutils.Rnw')
-texi2dvi('irutils.tex', pdf=TRUE)
 
---
+++ irutils.test.R
@@ -0,0 +1,44 @@
+theme_update(panel.background=theme_blank(), panel.grid.major=theme_blank(), panel.border=theme_blank())
+
+#Testing the plotting functions
+#How much do you agree or disagree with these statements about reading?
+data(pisa)
+
+items28 = pisa[,substr(names(pisa), 1,5) == 'ST24Q']
+head(items28); ncol(items28)
+names(items28) = c("I read only if I have to.",
+				   "Reading is one of my favorite hobbies.",
+				   "I like talking about books with other people.",
+				   "I find it hard to finish books.",
+				   "I feel happy if I receive a book as a present.",
+				   "For me, reading is a waste of time.",
+				   "I enjoy going to a bookstore or a library.",
+				   "I read only to get information that I need.",
+				   "I cannot sit still and read for more than a few minutes.",
+				   "I like to express my opinions about books I have read.",
+				   "I like to exchange books with my friends")
+for(i in 1:ncol(items28)) {
+	items28[,i] = factor(items28[,i], levels=1:4, 
+						 labels=c('Strongly disagree', 'Disagree', 'Agree', 'Strongly Agree'), ordered=TRUE)
+}
+
+plotBarchartTable(items28, low.color='maroon', high.color='burlywood4')
+plotBarchartTable(items28, grouping=pisa$CNT, low.color='maroon', high.color='burlywood4')
+plotBeatmapTable(items28)
+
+
+#How often do you read these materials because you want to?
+items29 = pisa[,substr(names(pisa), 1,5) == 'ST25Q']
+head(items29); ncol(items29)
+names(items29) = c("Magazines", "Comic books", "Fiction", "Non-fiction books", "Newspapers")
+for(i in 1:ncol(items29)) {
+	items29[,i] = factor(items29[,i], levels=1:5, 
+						 labels=c('Never or almost never', 'A few times a year', 'About once a month', 
+						 		 'Several times a month', 'Several times a week'), ordered=TRUE)
+}
+
+plotBarchartTable(items29, low.color='maroon', high.color='burlywood4') + 
+	opts(title="How often do you read these materials because you want to?")
+plotBarchartTable(items29, grouping=pisa$CNT, low.color='maroon', high.color='burlywood4') + 
+	opts(title="How often do you read these materials because you want to?")
+plotHeatmapTable(items29) + opts(title="How often do you read these materials because you want to?")

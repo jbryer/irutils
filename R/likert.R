04509d438f4acaea242929ee644f589809c4dd47
Jason Bryer <jason@bryer.org>
2012-02-27 15:36:22 -0500

    Moved vignette from inst/doc to vignettes directory. Updated likert methods.

--- R/likert.R
+++ R/likert.R
@@ -1,6 +1,23 @@
 #' Constructor function to create a likert class.
 #'
-#' TODO: Need more documentation 
+#' This function will provide various summary statistics about a set of likert
+#' items. The resulting object will have the following items:
+#' \itemize{
+#'    \item results this data frame will contain a column 'Item', 'Group' (if a 
+#'          grouping variable was specified, and a column for each level of the
+#'          items (e.g. agree, disagree, etc.). The value within each cell corresponds
+#'          to the percentage of responses for that level and group.
+#'    \item items a copy of the original items data frame.
+#'    \item grouping a copy of the original grouping vector.
+#'    \item nlevels the number of levels used in the calculations.
+#'    \item summary this data frame provides additional summary information. It
+#'          will contain 'Item' and 'Group' columns similiar to the results data
+#'          frame as well as a column 'low' corresponding to the sum of levels below
+#'          neutral, a column 'high' corresponding to the sum of levels above
+#'          neutral, and columns 'mean' and 'sd' corresponding to the mean and
+#'          standard deviation, respectively, of the results. The numeric values
+#'          are determined by as.numeric which will use the values of the factors.
+#' }
 #'
 #' @export
 #' @param items data frame containing the likert based items. The variables
@@ -8,6 +25,8 @@
 #' @param grouping (optional) should the results be summarized by the given
 #'        grouping variable.
 #' @param nlevels number of possible levels. Only necessary if there are missing levels.
+#' @return a likert class with the following elements: results, items, grouping,
+#'        nlevels, and summary.
 likert <- function(items, grouping=NULL, nlevels=length(levels(items[,1]))) {
 	lowrange = 1 : ceiling(nlevels / 2 - nlevels %% 2)
 	highrange = ceiling(nlevels / 2 + 1 ) : nlevels
@@ -31,47 +50,79 @@ likert <- function(items, grouping=NULL, nlevels=length(levels(items[,1]))) {
 		results2 = data.frame(Group=rep(unique(results$Group), each=ncol(items)),
 							  Item=rep(names(items), length(unique(results$Group))), 
 							  low=rep(NA, ncol(items) * length(unique(results$Group))), 
-							  high=rep(NA, ncol(items) * length(unique(results$Group))) )
+							  high=rep(NA, ncol(items) * length(unique(results$Group))),
+							  mean=rep(NA, ncol(items) * length(unique(results$Group))),
+							  sd=rep(NA, ncol(items) * length(unique(results$Group))) )
 		for(g in unique(results$Group)) {
-			results2[which(results2$Group == g),]$low = apply(results[results$Response %in% lowrange & results$Group == g,3:ncol(results)], 2, sum)
-			results2[which(results2$Group == g),]$high = apply(results[results$Response %in% highrange & results$Group == g,3:ncol(results)], 2, sum)
+			results2[which(results2$Group == g),]$low = apply(
+				results[results$Response %in% lowrange & 
+					results$Group == g,3:ncol(results)], 2, sum)
+			results2[which(results2$Group == g),]$high = apply(
+				results[results$Response %in% highrange & 
+					results$Group == g,3:ncol(results)], 2, sum)
+			for(i in names(items)) {
+				results2[which(results2$Group == g & results2$Item == i), 'mean'] = 
+					mean(as.numeric(items[which(grouping == g), i]), na.rm=TRUE)
+				results2[which(results2$Group == g & results2$Item == i), 'sd'] = 
+					sd(as.numeric(items[which(grouping == g), i]), na.rm=TRUE)
+			}
 		}
 		
+		results$Response = factor(results$Response, levels=1:nlevels, 
+								  labels=levels(items[,i]))
 		results = melt(results, id=c('Group', 'Response'))
-		results$Response = factor(results$Response, levels=1:nlevels, labels=levels(items[,i]))
-		names(results) = c('Group', 'Response', 'Item', 'Value')
+		results = cast(results, Group + variable ~ Response)
+		results = as.data.frame(results)
+		names(results)[2] = 'Item'
 	} else {
 		results = data.frame(Response=1:nlevels)
+		means = numeric()
+		sds = numeric()
 		for(i in 1:ncol(items)) {
 			t = table(items[,i])
 			t = (t / sum(t) * 100)
+			means[i] = mean(as.numeric(items[,i]), na.rm=TRUE)
+			sds[i] = sd(as.numeric(items[,i]), na.rm=TRUE)
 			results = cbind(results, as.data.frame(t)[2])
 			names(results)[ncol(results)] = names(items)[i]
 		}		
 		results = as.data.frame(t(results))
+		names(results) = as.character(unique(items[!is.na(items[,1]),1]))
 		results = results[2:nrow(results),]
-		results$item = row.names(results)
+		results$Item = row.names(results)
 		row.names(results) = 1:nrow(results)
-		results2 = data.frame(item=results$item, 
+		results2 = data.frame(Item=results$Item, 
 							  low=apply(results[,lowrange], 1, sum),
-							  high=apply(results[,highrange], 1, sum)
-							  )
-		results = melt(results, id.vars='item')
+							  high=apply(results[,highrange], 1, sum),
+							  mean=means, sd=sds)
+		#results = melt(results, id.vars='Item')
 	}
 	
-	r = list(results=results, items=items, grouping=grouping, nlevels=nlevels, results2=results2)
+	r = list(results=results, items=items, grouping=grouping, nlevels=nlevels, 
+			 summary=results2)
 	class(r) = 'likert'
 	return(r)
 }
 
-#' Prints summary table.
+#' Prints results table.
+#'
+#' @param x the likert class to print.
 #' @export
-#' @S3method plot likert
-print.likert <- function(likert, ...) {
-	cast(likert$results, item ~ variable)
+#' @S3method plot x
+print.likert <- function(x, ...) {
+	return(x$results)
 }
 
-#' Plots a set of likert items
+#' Prints summary table.
+#'
+#' @param x the likert class to summarize.
+#' @export
+#' #S3method summary x
+summary.likert <- function(x, ...) {
+	return(x$summary)
+}
+
+#' Plots a set of likert items. 
 #'
 #' @param likert the likert items to plot
 #' @param low.color color corresponding to the lowest value likert items
@@ -83,7 +134,8 @@ print.likert <- function(likert, ...) {
 #' @export
 #' @S3method plot likert
 plot.likert <- function(likert, low.color='blue', high.color='red', 
-			neutral.color='white', text.size=2, type=c('bar','heat'), ...)
+			neutral.color='white', text.colour='white', text.size=2, 
+			type=c('bar','heat'), ...)
 {
 	if(type[1] == 'bar') {
 		plot.likert.bar(likert, low.color=low.color, high.color=high.color,
@@ -94,8 +146,10 @@ plot.likert <- function(likert, low.color='blue', high.color='red',
 	}
 }
 
+#' Internal method.
+#' @seealso plot.likert
 plot.likert.bar <- function(likert, low.color='blue', high.color='red', 
-							neutral.color='white', text.size=2)
+							neutral.color='white', text.size=2, ...)
 {
 	lowrange = 1 : ceiling(likert$nlevels / 2 - likert$nlevels %% 2)
 	highrange = ceiling(likert$nlevels / 2 + 1 ) : likert$nlevels
@@ -111,25 +165,35 @@ plot.likert.bar <- function(likert, low.color='blue', high.color='red',
 	}
 	
 	if(!is.null(likert$grouping)) {
-		p = ggplot(likert$results, aes(y=Value, x=Group, group=Item)) + 
+		results = melt(likert$results, id=c('Group', 'Item'))
+		p = ggplot(results, aes(y=value, x=Group, group=variable)) + 
 			scale_fill_manual('Response', values=cols, 
-					breaks=unique(likert$results$Response),
+					breaks=unique(results$variable),
 					labels=levels(likert$items[,i])) + 
-			geom_bar(stat='identity', aes(fill=Response)) + 
-			geom_text(data=likert$results2, aes(x=Group, y=0, label=paste(round(low), '%', sep=''), group=Item), size=text.size, hjust=1) +
-			geom_text(data=likert$results2, aes(x=Group, y=100, label=paste(round(high), '%', sep=''), group=Item), size=text.size, hjust=-.2) +
+			geom_bar(stat='identity', aes(fill=variable)) + 
+			geom_text(data=likert$summary, aes(x=Group, y=0, 
+												label=paste(round(low), '%', sep=''), 
+												group=Item), size=text.size, hjust=1) +
+			geom_text(data=likert$summary, aes(x=Group, y=100, 
+												label=paste(round(high), '%', sep=''), 
+												group=Item), size=text.size, hjust=-.2) +
 			coord_flip() +
 			ylab('Percentage') + xlab('') + ylim(c(-5,105)) +
 			opts(axis.ticks=theme_blank()) +
 			facet_wrap(~ Item, ncol=1)
 	} else {
-		p = ggplot(likert$results, aes(y=value, x=item, group=item)) + 
+		results = melt(likert$results, id.vars='Item')
+		p = ggplot(results, aes(y=value, x=Item, group=Item)) + 
 			scale_fill_manual('Response', values=cols, 
-					breaks=unique(likert$results$variable), 
+					breaks=unique(results$variable), 
 					labels=levels(likert$items[,i])) + 
 			geom_bar(stat='identity', aes(fill=variable)) + 
-			geom_text(data=likert$results2, aes(x=item, y=0, label=paste(round(low), '%', sep='')), size=text.size, hjust=1) +
-			geom_text(data=likert$results2, aes(x=item, y=100, label=paste(round(high), '%', sep='')), size=text.size, hjust=-.2) +
+			geom_text(data=likert$summary, aes(x=Item, y=0, 
+												label=paste(round(low), '%', sep='')), 
+					  size=text.size, hjust=1) +
+			geom_text(data=likert$summary, aes(x=Item, y=100, 
+												label=paste(round(high), '%', sep='')), 
+					  size=text.size, hjust=-.2) +
 			coord_flip() +
 			ylab('Percentage') + xlab('') + ylim(c(-5,105)) +
 			opts(axis.ticks=theme_blank())
@@ -137,16 +201,28 @@ plot.likert.bar <- function(likert, low.color='blue', high.color='red',
 	return(p)
 }
 
-#' Function creates and returns a tabular heatmap ideal for summarizing likert
-#' survey items.
-#' @param items the items to summarize.
-#' @export
-plot.likert.heat <- function(likert, low.color='white', high.color='blue', text.size=2) {	
-	p = ggplot(likert$results, aes(x=item, y=variable, fill=value, label=paste(value, '%', sep=''))) + 
-		geom_text(aes(y='Mean (SD)', x=item, label=paste(format(mean, digits=3, drop0trailing=FALSE), ' (', format(sd, digits=2, drop0trailing=FALSE), ')', sep='')), size=text.size) +
-		geom_tile() + geom_text(size=text.size) + coord_flip() + 
+#' Internal method.
+#' @seealso plot.likert
+plot.likert.heat <- function(likert, low.color='blue', high.color='red', 
+				neutral.color='white', text.colour='white', text.size=2, ...) {
+	if(!is.null(likert$grouping)) {
+		stop('likert plots with grouping are not supported.')
+	}
+
+	results = melt(likert$results, id.vars='Item')
+	
+	p = ggplot(results, aes(x=Item, y=variable, fill=value, 
+								   label=paste(format(value, digits=2, 
+								   		drop0trailing=FALSE), '%', sep=''))) + 
+ 		geom_text(data=likert$summary, aes(y='Mean (SD)', x=Item, fill='white',
+ 				  label=paste(format(mean, digits=3, drop0trailing=FALSE), 
+ 							' (', format(sd, digits=2, drop0trailing=FALSE), ')', sep='')), 
+ 				  size=text.size) +
+		geom_tile() + geom_text(size=text.size, colour=text.colour) + coord_flip() + 
 		scale_fill_gradient("Percent", low=low.color, high=high.color, limits=c(0,100)) + 
 		xlab('') + ylab('') + 
-		opts(panel.grid.major=theme_blank(), panel.grid.minor=theme_blank(), axis.ticks=theme_blank())
+		opts(panel.grid.major=theme_blank(), 
+			 panel.grid.minor=theme_blank(), 
+			 axis.ticks=theme_blank())
 	return(p)
 }
